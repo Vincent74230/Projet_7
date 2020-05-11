bf5a6cc4180b4bbabf2057cfaae2afd62c82d770
Vincent74230 <vincent.nowak@hotmail.fr>
2020-05-11 15:31:37 +0200

    TRIAL with google static map api: working

--- dynamic.js
+++ dynamic.js
@@ -1,28 +1,38 @@
 
 // google maps api key = AIzaSyBvjVqsDCXUG3xGNdZai3lQBkekuMbUAfk
+// api request example = "https://maps.googleapis.com/maps/api/staticmap?center=pyramide de gizeh&zoom=14&size=400x400&key=AIzaSyBvjVqsDCXUG3xGNdZai3lQBkekuMbUAfk"
 
 let button = document.getElementById("button");
 let form = document.getElementById("form");
-
 let text = document.getElementById("question");
+var myString = '';
+var googleMap = document.getElementById("googleMap");
+var img = document.createElement("img");
 
 form.addEventListener("submit", function(e){
-    var quest = text.value;
-    console.log(e,quest);
 
     e.preventDefault();
+    var myString1 = "https://maps.googleapis.com/maps/api/staticmap?center=";
+    var myString2 = "&zoom=14&size=400x400&key=AIzaSyBvjVqsDCXUG3xGNdZai3lQBkekuMbUAfk";
+    myString = myString1+text.value+myString2;
+
+    img.src = myString;
+    googleMap.appendChild(img);
+    text.value = '';
 });
 
-
-var request = new XMLHttpRequest();
-request.onreadystatechange = function(){
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200){
-        var response = request.response;
-
+/*
+var map;
+var xhr = new XMLHttpRequest();
+xhr.addEventListener('readystatechange', function(){
+    if (xhr.readyState === 4 && xhr.status === 200){
+        map = xhr;
     }
-}
+        
+});
 
-request.open('get', "https://maps.googleapis.com/maps/api/staticmap?center=Berkeley,CA&zoom=14&size=400x400&key=AIzaSyBvjVqsDCXUG3xGNdZai3lQBkekuMbUAfk");
-request.send();
+xhr.open('get', "https://maps.googleapis.com/maps/api/staticmap?center=Chicago&zoom=14&size=400x400&key=************************");
+xhr.send();
+*/
 
-console.log(request);
+
