073c0ad7511e1b2be88d722ec832e7799f69550e
Vincent74230 <vincent.nowak@hotmail.fr>
2020-05-26 17:54:31 +0200

    STARTING to build geoloc module

--- grandpy/get_position.py
+++ grandpy/get_position.py
@@ -1,1 +1,15 @@
 #coding : UTF-8
+import requests
+
+def get_position(location):
+    payload = {'address':location,'key':'AIzaSyBvjVqsDCXUG3xGNdZai3lQBkekuMbUAfk'}
+    r = requests.get('https://maps.googleapis.com/maps/api/geocode/json', params=payload)
+
+    print(r.url)
+    r = r.json()
+    r = r['results']
+    print(r)
+    print(r[1])
+
+
+get_position('paris')
