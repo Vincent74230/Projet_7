adc047a3682a72c869ccbc88b4e4ad6db3b4ae1b
Vincent74230 <vincent.nowak@hotmail.fr>
2020-06-05 13:23:58 +0200

    REORGANIZED the project: added a run.py and updated __init__

--- grandpy/views.py
+++ grandpy/views.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
-from parser import parser
-from get_position import get_position
-from get_wiki_summary import get_wiki_summary
+from .parser import parser
+from .get_position import get_position
+from .get_wiki_summary import get_wiki_summary
 
 app = Flask(__name__)
 
@@ -15,6 +15,3 @@ def search():
         users_question = request.form['question']
         place_in_the_world = parser(users_question)
         return {'geoloc':get_position(place_in_the_world),'bla_bla':get_wiki_summary(place_in_the_world)}
-
-if __name__ == '__main__':
-    app.run(debug = True)
